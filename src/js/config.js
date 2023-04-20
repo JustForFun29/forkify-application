8bece147a6302ac210fbea89e74e219065d5866c
justforfun29 <dimashumbetzhan@gmail.com>
2023-04-21 04:17:18 +0600

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const API_KEY = '6224fe15-39c9-4e29-9468-45a83f8f8745';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const MODAL_CLOSE_SEC = 2.5;
